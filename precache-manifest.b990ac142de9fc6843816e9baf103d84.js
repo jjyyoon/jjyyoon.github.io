fb596bf0d82876feefa1fa765ac86334dcfa93cc
Jiyeon Yoon <jjjy.yoon@gmail.com>
2020-02-29 10:50:03 +0000

    Updates

---
+++ precache-manifest.b990ac142de9fc6843816e9baf103d84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c052a766d1467046a35910c24f8c4eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63f94d9256d0908d0aea",
+    "url": "/static/css/main.5a35acc2.chunk.css"
+  },
+  {
+    "revision": "acfd2a05d2ca72a95b36",
+    "url": "/static/js/2.940577c8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.940577c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f94d9256d0908d0aea",
+    "url": "/static/js/main.055c4c83.chunk.js"
+  },
+  {
+    "revision": "af43a8a193db17ea48b2",
+    "url": "/static/js/runtime-main.ffd339f4.js"
+  },
+  {
+    "revision": "3508ef17ace617d8e561c1e8d96c4f86",
+    "url": "/static/media/homepage.3508ef17.svg"
+  }
+]);
