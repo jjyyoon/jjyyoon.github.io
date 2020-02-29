74dcd32f7070baabed5c9c1db516cbc0b17efef2
Jiyeon Yoon <jjjy.yoon@gmail.com>
2020-02-29 12:03:17 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { ReactComponent as HomepageImg } from "./assets/homepage.svg";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>This is the homepage of my tic-tac-toe-online app.</p>
+      <HomepageImg />
     </div>
   );
 }
